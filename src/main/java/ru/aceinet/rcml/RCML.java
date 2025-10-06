dc49dc9579a0979f7b2014f26cc6691c5d38237e
aceinetx <mouselin82@gmail.com>
2025-10-06 18:07:37 +0300

    add main class & plugin.yml

--- src/main/java/ru/aceinet/rcml/RCML.java
+++ src/main/java/ru/aceinet/rcml/RCML.java
@@ -2,10 +2,18 @@ package ru.aceinet.rcml;
 
 import net.kyori.adventure.text.Component;
 
-public class RCML {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RCML extends JavaPlugin {
     public static Component compile(String code, Object... args) {
         Parser parser = new Parser(String.format(code, args));
         Compiler compiler = new Compiler(parser);
         return compiler.compile();
+    }
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(compile("Thank you for using [bold:true][color:255,0,0]RCML[reset]!"));
     }
 }
